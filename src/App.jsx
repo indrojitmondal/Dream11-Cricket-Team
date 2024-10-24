98d6a0a813300b9a268edd218997da9395c373be
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-24 22:06:02 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +10,8 @@ function App() {
   return (
     <>
       <header className='w-11/12 mx-auto pt-4'>
-             <h1 className='text-xl font-bold'>Welcome Dream11 Cricket Team</h1>
-             <button className='btn'>Lets Start</button>
+            
+             <Navbar></Navbar>
       </header>
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between'>
+
+            <div className="">
+                <img className='w-[70px] h-[60px]' src="../../../src/assets/logo.png" alt="" />
+            </div>
+            <menu className='flex gap-5 items-center'>
+                <div className='flex gap-5'>
+
+                    <a href="">Home</a>
+                    <a href="">Fixture</a>
+                    <a href="">Teams</a>
+                    <a href="">Schedules</a>
+
+                </div>
+
+                <div className='flex items-center gap-4'>
+                    <p className='font-semibold'>0 Coin</p>
+                    <img src="../../../src/assets/coin.png" alt="" />
+                </div>
+            </menu>
+
+        </div>
+    );
+};
+
+export default Navbar;
