185a222cf88c7623c750d212eebbe4a2a59c1717
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-24 20:19:52 +0600

    Configured Tailwindcss, daisyUI and reactjs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='w-11/12 mx-auto pt-4'>
+             <h1 className='text-xl font-bold'>Welcome to Assignment7</h1>
+             <button className='btn'>Lets Start</button>
+      </header>
+    </>
+  )
+}
+
+export default App
