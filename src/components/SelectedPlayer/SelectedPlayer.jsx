25b94196c01c13e15ba68d35279342d95cd7236d
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-25 19:13:13 +0600

    updated selectedPlayer.jsx

--- src/components/SelectedPlayer/SelectedPlayer.jsx
+++ src/components/SelectedPlayer/SelectedPlayer.jsx
@@ -3,7 +3,7 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 
 const SelectedPlayer = ({ player }) => {
     console.log('Ok:', player);
-    const { player_img, name, role, price } = player;
+    const { player_img, name, role, price,batting_type,bowling_type } = player;
     return (
         <div className=' flex justify-between   border border-gray-200 rounded-xl p-5'>
 
@@ -16,7 +16,9 @@ const SelectedPlayer = ({ player }) => {
 
                     <h3 className='font-semibold text-xl'>{name}</h3>
 
-                    <h4>{role}</h4>
+                    <h4>{role}</h4> 
+                    <h4>{batting_type}</h4> 
+                    <h4>{bowling_type}</h4> 
                     <h5 className='font-semibold'>Price: ${price}</h5>
 
                 </div>
