9c28ac166c898a2761e4d25566bafdfc8d3591dc
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-26 11:54:29 +0600

    Desined footer exact figma

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,26 +5,33 @@ const Footer = () => {
         <>
    <footer className="mt-20 relative ">
                 {/* Upper section */}
-                <div className="upper  absolute z-50 rounded-xl footer_upper w-11/12 mx-auto top-0 left-0 right-0  p-16  ">
+                <div className="upper border  border-white absolute z-50 rounded-xl  w-11/12 mx-auto top-0 left-0 right-0  px-4 pb-4 pr-4 ">
+
+                <div className=" rounded-xl bg-f1  footer_upper   ">
                    
-                <div className="p-4   text-center space-y-3">
+                <div className="p-8   text-center space-y-3">
                         <h6 className="text-3xl font-bold">Subscribe to our Newsletter</h6>
                         <p className="text-xl font-medium">Get the latest updates and news right in your inbox!</p>
                         <div className="join flex justify-center gap-4">
                             <input type="text" placeholder="Enter your email" className="input input-bordered" />
-                            <button className="gradient-button">Subscribe</button>
+                             <button className="gradient-button">Subscribe</button>
                         </div>
-                    </div>
-                
-                
                 </div>
+                
+                
+                
+                  
+                  
+                  </div>
+                </div>
+               
 
                 {/* Downer section */}
 
-                <div className="downer  top-28 absolute z-20 bg-black w-full">
-                    <div className="text-white bg-white pt-[180px]   w-11/12 mx-auto">
+                <div className="downer  top-20 absolute z-20 bg-black w-full">
+                    <div className="text-white bg-white    w-11/12 mx-auto">
                         {/* start from here */}
-                        <div className='text-white pt-5 bg-black'>
+                        <div className='text-white pt-[180px] bg-black'>
 
                             <img className='mx-auto' src="https://i.ibb.co.com/bvvGH8G/logo-footer.png" alt="" />
 
